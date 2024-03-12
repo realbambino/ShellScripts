04a37807709ceb5251b74c3e5a441218f971729d
Ino Bambino <104255681+realbambino@users.noreply.github.com>
2024-03-12 18:02:49 +0700

    Download best video using YT-DLP

---
+++ yt-video.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo Downloading files, please wait...
+yt-dlp -a download.txt -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' --sub-langs all,-live_chat --embed-subs --embed-chapters --sponsorblock-mark all --merge-output-format mkv --merge-output-format mkv -o "%(title)s.%(ext)s"
+echo
+echo Download complete.
