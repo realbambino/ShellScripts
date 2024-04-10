bae193c9c091856b82e6a70eff351918fb6d6f9c
Ino Bambino <104255681+realbambino@users.noreply.github.com>
2024-04-10 21:40:40 +0700

    Update yt-section.sh
    Added some color and information to the script.

--- yt-section.sh
+++ yt-section.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Define color escape codes
+WHITE='\033[0;37m'
+NC='\033[0m' # No Color
+YELLOW='\033[0;33m'
+
 # Check if all arguments are provided
 if [ $# -ne 3 ]; then
 	echo "Usage: $0 <start_time> <end_time> <video_id>"
@@ -20,6 +25,12 @@ fi
 start_time="$1"
 end_time="$2"
 video_id="$3"
+
+# Adding some screen information to the user
+echo -e "${YELLOW}INFO:${NC} Downloading section from ID: $video_id"
+echo -e "${YELLOW}INFO:${NC} Preparing to download"
+echo -e "${YELLOW}INFO:${NC} Download section(s) from timestamp ${WHITE}start_time${NC} and ${WHITE}end_time${NC}"
+echo -e "${YELLOW}INFO:${NC} Loading modules, please wait..."   
 
 # Download specific section of the video
 yt-dlp --download-sections *0:$start_time-0:$end_time -f "bestvideo[height<=2160][vcodec^=hevc][protocol!*=dash]/bestvideo[height<=2160][vcodec^=avc1][protocol!*=dash]+bestaudio[acodec^=mp4a]/best[height<=2160]" --merge-output-format mp4 --audio-quality 0 -o "%(title)s.%(ext)s" "https://www.youtube.com/watch?v=$video_id"
