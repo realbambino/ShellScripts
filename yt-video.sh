b1fde1ebe4c77f9f9c9d4d8b9190aa71a7381557
Ino Bambino <104255681+realbambino@users.noreply.github.com>
2024-04-10 21:43:06 +0700

    Update yt-video.sh
    Added some color and information to the script.

--- yt-video.sh
+++ yt-video.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo Downloading files, please wait...
+WHITE='\033[0;37m'
+NC='\033[0m' # No Color
+YELLOW='\033[0;33m'
+
+echo -e "${YELLOW}INFO:${NC} Opening download list"
+echo -e "${YELLOW}INFO:${NC} Preparing to download"
+echo -e "${YELLOW}INFO:${NC} Loading modules, please wait..."
 yt-dlp -a download.txt -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' --sub-langs all,-live_chat --embed-subs --embed-chapters --sponsorblock-mark all --merge-output-format mkv -o "%(title)s.%(ext)s"
-echo
-echo Download complete.
+echo -e "${YELLOW}INFO:${NC} ${WHITE}Download complete${NC}"
