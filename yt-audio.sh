b3659c796da98ef8a376cd7f98dbf775ce8fd94a
Ino Bambino <104255681+realbambino@users.noreply.github.com>
2024-04-10 21:44:55 +0700

    Update yt-audio.sh
    Added some color and information to the script. Change the codec to download to AAC.

--- yt-audio.sh
+++ yt-audio.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-echo Downloading files, please wait...
-yt-dlp -a download.txt -x -f "bestaudio" --audio-format mp3 --audio-quality 192K -o "%(title)s.%(ext)s" 
-echo
-echo Download complete.
+# Define color escape codes
+WHITE='\033[0;37m'
+NC='\033[0m' # No Color
+YELLOW='\033[0;33m'
+
+echo -e "${YELLOW}INFO:${NC} Opening download list"
+echo -e "${YELLOW}INFO:${NC} Preparing to download"
+echo -e "${YELLOW}INFO:${NC} Loading modules, please wait..."
+
+# Download AAC files, no need to convert. Saving time in the process.
+yt-dlp -a download.txt -x -f "bestaudio[acodec^=mp4a]" --audio-format m4a -o "%(title)s.%(ext)s"
+echo -e "${YELLOW}INFO:${NC} ${WHITE}Download complete${NC}" 
