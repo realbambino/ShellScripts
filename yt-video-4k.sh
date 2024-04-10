2acbe0147e1ba135ed57dff9e9764799a94e6ea1
Ino Bambino <104255681+realbambino@users.noreply.github.com>
2024-04-10 21:41:49 +0700

    Update yt-video-4k.sh
    Added some color and information to the script.

--- yt-video-4k.sh
+++ yt-video-4k.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo Downloading files, please wait...
+WHITE='\033[0;37m'
+NC='\033[0m' # No Color
+YELLOW='\033[0;33m'
+
+echo -e "${YELLOW}INFO:${NC} Opening download list"
+echo -e "${YELLOW}INFO:${NC} Preparing to download"
+echo -e "${YELLOW}INFO:${NC} Loading modules, please wait..."
 yt-dlp -a download.txt -f "bestvideo[height<=2160]+bestaudio[acodec^=mp4a]/best[height<=2160]" --merge-output-format mkv --audio-quality 0 --sub-langs all,-live_chat --embed-subs --embed-chapters --sponsorblock-mark all --merge-output-format mkv -o "%(title)s (4K).%(ext)s"
-echo
-echo Download complete.
+echo -e "${YELLOW}INFO:${NC} ${WHITE}Download complete${NC}" 
