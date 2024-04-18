2b1c34611af2e0c75ca1ad3541db6d70429a99ad
Ino Bambino <104255681+realbambino@users.noreply.github.com>
2024-04-19 02:36:12 +0700

    Update yt-section.sh
    -Added the DASH protocol to be used when downloading
    -Changed the video format from MP4 to MKV for compatibility reasons.

--- yt-section.sh
+++ yt-section.sh
@@ -91,11 +91,11 @@ echo -e "${YELLOW}INFO:${NC} Set download section from timestamp: ${WHITE}$start
 echo -e "${YELLOW}INFO:${NC} Loading modules, please wait..."
 
 # Download specific section of the video
-yt-dlp --download-sections *0:$start_time-0:$end_time -f "bestvideo[height<=2160][protocol!*=dash]+bestaudio/best[height<=2160]" -o "downloaded_video.%(ext)s" "$full_link"
+yt-dlp --download-sections *0:$start_time-0:$end_time -f "bestvideo[height<=2160][protocol!*=dash]+bestaudio/best[height<=2160][protocol!*=dash]" -o "downloaded_video.%(ext)s" "$full_link"
 
 # Convert the audio to AAC using FFMPEG
 echo -e "${YELLOW}INFO:${NC} Converting audio to ${WHITE}AAC${NC}"
-ffmpeg -i 'downloaded_video.webm' -c:v copy -c:a aac -loglevel quiet -strict experimental 'downloaded_video.mp4'
+ffmpeg -i 'downloaded_video.webm' -c:v copy -c:a aac -loglevel quiet -strict experimental 'downloaded_video.mkv'
 
 # Removing the old video
 echo -e "${YELLOW}INFO:${NC} Cleaning up"
