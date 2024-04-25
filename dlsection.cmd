4c33c4c404f30dd7c4bb0775a03a7a1a1fdf35b8
Ino Bambino <104255681+realbambino@users.noreply.github.com>
2024-04-25 20:44:06 +0700

    Update dlsection.cmd
    -Change an input into a string

--- dlsection.cmd
+++ dlsection.cmd
@@ -1,6 +1,6 @@
 @echo off
 echo Downloading video...
 echo.
-yt-dlp --download-sections *0:%1-0:%2 -f "bestvideo[height<=2160][vcodec^=avc1][protocol!*=dash]+bestaudio[acodec^=mp4a]/best[height<=2160]" --merge-output-format mp4 --audio-quality 0 -o "%(title)s.%(ext)s" %3
+yt-dlp --download-sections "*0:%1-0:%2" -f "bestvideo[height<=2160][vcodec^=avc1][protocol!*=dash]+bestaudio[acodec^=mp4a]/best[height<=2160]" --merge-output-format mp4 --audio-quality 0 -o "%(title)s.%(ext)s" %3
 echo.
 echo Download completed.
